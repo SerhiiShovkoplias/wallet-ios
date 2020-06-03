959bb2a386f50cc557442f5a4b0c60ab9f842064
SerhiiShovkoplias <s.shovkoplyas@newlinetechnologies.net>
2020-06-03 15:49:34 +0300

    added "add word" and "restore word" to recoveryPhraseView

--- MobileWallet/UIElements/RecoveryPhraseView.swift
+++ MobileWallet/UIElements/RecoveryPhraseView.swift
@@ -1,47 +1,47 @@
 //  RecoveryPhraseView.swift
 
 /*
-	Package MobileWallet
-	Created by S.Shovkoplyas on 01.06.2020
-	Using Swift 5.0
-	Running on macOS 10.15
-
-	Copyright 2019 The Tari Project
-
-	Redistribution and use in source and binary forms, with or
-	without modification, are permitted provided that the
-	following conditions are met:
-
-	1. Redistributions of source code must retain the above copyright notice,
-	this list of conditions and the following disclaimer.
-
-	2. Redistributions in binary form must reproduce the above
-	copyright notice, this list of conditions and the following disclaimer in the
-	documentation and/or other materials provided with the distribution.
-
-	3. Neither the name of the copyright holder nor the names of
-	its contributors may be used to endorse or promote products
-	derived from this software without specific prior written permission.
-
-	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
-	CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
-	INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-	OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-	DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
-	CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-	SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
-	NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-	HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-	CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-	OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+ Package MobileWallet
+ Created by S.Shovkoplyas on 01.06.2020
+ Using Swift 5.0
+ Running on macOS 10.15
+ 
+ Copyright 2019 The Tari Project
+ 
+ Redistribution and use in source and binary forms, with or
+ without modification, are permitted provided that the
+ following conditions are met:
+ 
+ 1. Redistributions of source code must retain the above copyright notice,
+ this list of conditions and the following disclaimer.
+ 
+ 2. Redistributions in binary form must reproduce the above
+ copyright notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ 
+ 3. Neither the name of the copyright holder nor the names of
+ its contributors may be used to endorse or promote products
+ derived from this software without specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 import UIKit
 
 protocol RecoveryPhraseViewDelegate: class {
-    func didSelectWord(word: String, index: Int, phraseView: RecoveryPhraseView)
+    func didSelectWord(word: String, intId: Int, phraseView: RecoveryPhraseView)
 }
 
 class RecoveryPhraseView: UIView {
@@ -128,9 +128,44 @@ class RecoveryPhraseView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
 
+    func addWord(_ word: String, intId: Int) {
+        let newButton = createButton(word: word, intId: intId)
+
+        buttons.append(newButton)
+        words.append(word)
+
+        if let lastStackView = findLastFreeSubStackView(for: newButton) {
+            var existingButtons = lastStackView.subviews.filter({ $0 is UIButton })
+            existingButtons.append(newButton)
+            stackView.removeArrangedSubview(lastStackView)
+            subStackViews.removeAll(where: { $0 == lastStackView })
+
+            let newStack = horizontalStackView(with: existingButtons, intrinsicWidth: bounds.width)
+            subStackViews.append(newStack)
+            stackView.addArrangedSubview(newStack)
+        } else {
+            let newStackView = horizontalStackView(with: [newButton], intrinsicWidth: bounds.width)
+            subStackViews.append(newStackView)
+            stackView.addArrangedSubview(newStackView)
+        }
+
+        heightConstraint?.constant = heightForStackView()
+    }
+
+    func restore(word: String, intId: Int) {
+        if type != .selectable { return }
+        if let button = buttons.first(where: {$0.tag == intId}) {
+            if button.titleLabel?.text == word {
+                UIView.animate(withDuration: CATransaction.animationDuration()) {
+                    button.alpha = 1.0
+                }
+            }
+        }
+    }
+    
     @objc private func buttonAction(_ sender: UIButton) {
         guard let word = sender.titleLabel?.text, let index = buttons.firstIndex(of: sender) else { return }
-        delegate?.didSelectWord(word: word, index: index, phraseView: self)
+        delegate?.didSelectWord(word: word, intId: sender.tag, phraseView: self)
 
         UIView.animate(withDuration: CATransaction.animationDuration(), animations: {
             sender.alpha = 0.0
@@ -139,36 +174,25 @@ class RecoveryPhraseView: UIView {
             switch self.type {
             case .fillable: do {
                 self.words.remove(at: index)
+                self.buttons.remove(at: index)
                 self.stackView.removeFromSuperview()
+
                 self.stackView = UIStackView()
-                self.setup(words: self.words, width: self.bounds.width)
+                self.subStackViews = self.createSubStackViews(intrinsicWidth: self.bounds.width)
+                self.setupStackView(horizontalStackViews: self.subStackViews)
                 }
             default: break
             }
         }
     }
+}
 
-    func addWords(_ words: [String]) {
-        let newButtons = createButtons(words: words)
-
-        newButtons.forEach { (button) in
-            guard let lastStackView = findLastFreeSubStackView(for: button) else {
-                let newStackView = horizontalStackView(with: [button], intrinsicWidth: bounds.width)
-                subStackViews.append(newStackView)
-                stackView.addArrangedSubview(newStackView)
-                return
-            }
-            var existingButtons = lastStackView.subviews.filter({ $0 is UIButton })
-            existingButtons.append(button)
-            stackView.removeArrangedSubview(lastStackView)
-            subStackViews.removeAll(where: { $0 == lastStackView })
-            let newStack = horizontalStackView(with: existingButtons, intrinsicWidth: bounds.width)
-            subStackViews.append(newStack)
-            stackView.addArrangedSubview(newStack)
-        }
-        buttons += newButtons
-        self.words += words
-        heightConstraint?.constant = heightForStackView()
+// MARK: Private Methods
+extension RecoveryPhraseView {
+    private func setup(words: [String], width: CGFloat) {
+        buttons = createButtons(words: words)
+        subStackViews = createSubStackViews(intrinsicWidth: width)
+        setupStackView(horizontalStackViews: subStackViews)
     }
 
     private func findLastFreeSubStackView(for button: UIButton) -> UIStackView? {
@@ -192,35 +216,33 @@ class RecoveryPhraseView: UIView {
             return nil
         }
     }
-
-    private func setup(words: [String], width: CGFloat) {
-        buttons = createButtons(words: words)
-        subStackViews = createSubStackViews(intrinsicWidth: width)
-        setupStackView(horizontalStackViews: subStackViews)
+    private func createButtons(words: [String]) -> [UIButton] {
+        var buttons = [UIButton]()
+        words.forEach({
+            buttons.append(createButton(word: $0, intId: UUID().hashValue))
+        })
+        return buttons
     }
 
-    private func createButtons(words: [String]) -> [UIButton] {
-        var intrinsicButtons = [UIButton]()
-        words.forEach({
-            let button = UIButton()
-            button.addTarget(self, action: #selector(buttonAction(_:)), for: .touchUpInside)
-            button.setTitle($0, for: .normal)
-            button.titleLabel?.font = font
-            button.backgroundColor = textBackgroundColor
-            button.setTitleColor(textColor, for: .normal)
-            button.setTitleColor(textColor.withAlphaComponent(0.5), for: .highlighted)
+    private func createButton(word: String, intId: Int) -> UIButton {
+        let button = UIButton()
+        button.addTarget(self, action: #selector(buttonAction(_:)), for: .touchUpInside)
+        button.setTitle(word, for: .normal)
+        button.titleLabel?.font = font
+        button.tag = intId
+        button.backgroundColor = textBackgroundColor
+        button.setTitleColor(textColor, for: .normal)
+        button.setTitleColor(textColor.withAlphaComponent(0.5), for: .highlighted)
 
-            button.layer.cornerRadius = cornerRadius
-            button.layer.borderColor = showBorder ? buttonBorderColor.cgColor : UIColor.clear.cgColor
-            button.layer.borderWidth = 1.0
-            button.layer.masksToBounds = true
-            let widthConstraint =  button.widthAnchor.constraint(equalToConstant: ((button.titleLabel?.intrinsicContentSize.width ?? 0.0) + minimumInsets.left + minimumInsets.right))
-            widthConstraint.isActive = true
-            widthConstraint.priority = .defaultHigh
+        button.layer.cornerRadius = cornerRadius
+        button.layer.borderColor = showBorder ? buttonBorderColor.cgColor : UIColor.clear.cgColor
+        button.layer.borderWidth = 1.0
+        button.layer.masksToBounds = true
+        let widthConstraint =  button.widthAnchor.constraint(equalToConstant: ((button.titleLabel?.intrinsicContentSize.width ?? 0.0) + minimumInsets.left + minimumInsets.right))
+        widthConstraint.isActive = true
+        widthConstraint.priority = .defaultHigh
 
-            intrinsicButtons.append(button)
-        })
-        return intrinsicButtons
+        return button
     }
 
     private func createSubStackViews(intrinsicWidth: CGFloat) -> [UIStackView] {
@@ -322,14 +344,7 @@ class RecoveryPhraseView: UIView {
     }
 
     private func heightForStackView() -> CGFloat {
-        var labelHeight: CGFloat
-
-        if buttons.first?.bounds.height == 0 {
-            labelHeight = (buttons.first?.titleLabel?.font.pointSize ?? 0.0) + minimumInsets.top + minimumInsets.bottom
-        } else {
-            labelHeight = buttons.first?.bounds.height ?? 0.0
-        }
-
+        let labelHeight = (buttons.first?.titleLabel?.font.pointSize ?? 0.0) + minimumInsets.top + minimumInsets.bottom
         let height: CGFloat = CGFloat(subStackViews.count) * labelHeight + CGFloat(subStackViews.count - 1) * stackView.spacing
         return height < minimumHeight ? minimumHeight : height
     }
